98cd50b86ca35033e957ca6f4a9a32c52434fb9d
Tina <tina.zhou.hui@gmail.com>
2022-06-09 14:44:09 +0100

    feat: playmastermind

--- __tests__/livecode.js
+++ __tests__/livecode.js
@@ -1,5 +1,5 @@
 import {describe, test, expect} from '@jest/globals';
-import {getNested} from '../src/livecode.js';
+import {getNested, playMastermind} from '../src/livecode.js';
 
 describe('Tests', () => {
   const obj = {
@@ -21,4 +21,19 @@ describe('Tests', () => {
   test('nested object should return undefined if value not found', () => {
     expect(getNested(obj, 'a.b.d', '123')).toBe('123');
   });
+
+  describe('PlayMastermind', () => {
+    test('should return correctly when guessed both position and color', () => {
+      const toGuess = ['red', 'blue', 'green'];
+      const guesses = ['red', 'yellow', 'green'];
+      expect(playMastermind(toGuess, guesses)).toStrictEqual([2, 0]);
+    });
+
+    test('should return correctly when guessed position and color', () => {
+      const toGuess = ['purple', 'cyan', 'magenta'];
+      const guesses = ['magenta', 'cyan', 'purple'];
+      expect(playMastermind(toGuess, guesses)).toStrictEqual([1, 2]);
+    });
+  });
+
 });
--- src/livecode.js
+++ src/livecode.js
@@ -8,9 +8,40 @@ export function getNested (obj, path, def = undefined) {
   const pathElements = path.split('.');
   for (const pathElement of pathElements) {
     // loop through the pathElements and access object
-    obj = obj[pathElement]
+    obj = obj[pathElement];
     // if not - return undefined
     if (obj === undefined) return def;
   }
   return obj;
+}
+
+// color guessing game
+// playMastermind(['red', 'blue', 'green'], ['red', 'yellow', 'green']);
+// > [2, 0]
+//
+// playMastermind(['purple', 'cyan', 'magenta'], ['magenta', 'cyan', 'purple']);
+// > [1, 2]
+
+export function playMastermind (toGuess, guesses) {
+  // get the correct position and value
+  let numberOfColors = toGuess.length;
+  let bothCorrect = 0;
+  let valueCorrect = 0;
+  for (let i = 0; i < numberOfColors; i++) {
+    if (toGuess[i] === guesses[i]) {
+      bothCorrect += 1;
+      // delete them from both arrays
+      toGuess.splice(i, 1);
+      guesses.splice(i, 1);
+      i--;
+      numberOfColors--;
+    }
+  }
+  // what is left in toGuess includes any of the left guesses
+  for (const color of guesses) {
+    if (toGuess.includes(color)) {
+      valueCorrect += 1;
+    }
+  }
+  return [bothCorrect, valueCorrect];
 }
