34f5e95dbe8a4378e598b2a258454922f5a8ded9
Tina <tina.zhou.hui@gmail.com>
2022-06-09 14:25:12 +0100

    feat: getNested

--- __tests__/livecode.js
+++ __tests__/livecode.js
@@ -1,7 +1,24 @@
 import {describe, test, expect} from '@jest/globals';
+import {getNested} from '../src/livecode.js';
 
 describe('Tests', () => {
-  test('dummy test', () => {
-    expect(1).toEqual(1)
+  const obj = {
+    a: {
+      b: {
+        c: 'hello'
+      }
+    }
+  };
+
+  test('nested object should return existing value based on path', () => {
+    expect(getNested(obj, 'a.b.c')).toEqual('hello');
+  });
+
+  test('nested object should return existing value', () => {
+    expect(getNested(obj, 'a.b')).toStrictEqual({c: 'hello'});
+  });
+
+  test('nested object should return undefined if value not found', () => {
+    expect(getNested(obj, 'a.b.d', '123')).toBe('123');
   });
 });
---
+++ src/livecode.js
@@ -0,0 +1,16 @@
+/**
+ * @param obj
+ * @param {string} path example 'a.b.c'
+ * @param def default value if not found
+ */
+export function getNested (obj, path, def = undefined) {
+  // parse the string -> pathElements
+  const pathElements = path.split('.');
+  for (const pathElement of pathElements) {
+    // loop through the pathElements and access object
+    obj = obj[pathElement]
+    // if not - return undefined
+    if (!obj) return def;
+  }
+  return obj;
+}
